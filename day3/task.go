02ac953d715b716e828cadc1a1a7eecef57ffb14
Mészáros Gergely <gmeszaros@cloudera.com>
2024-12-03 13:38:33 +0100

    day 3 part 2 complete

--- day3/task.go
+++ day3/task.go
@@ -11,12 +11,22 @@ import (
 type Day3 struct{}
 
 const (
-	mainPattern       = "(mul\\(\\d*,\\d*\\))"
-	numberPairPattern = "\\d*,\\d*"
-	numberSeparator   = ","
+	mainPattern            = "(mul\\(\\d*,\\d*\\))"
+	conditionalMainPattern = "(mul\\(\\d*,\\d*\\))|(don\\'t\\(\\))|(do\\(\\))"
+	numberPairPattern      = "\\d*,\\d*"
+	numberSeparator        = ","
 )
 
 func (Day3) CompleteTask(input []string) {
+	sum := getTotalSum(input)
+	conditionalSum := getConditionalSum(input)
+	fmt.Println("---------------")
+	fmt.Printf("Sum: %d%s", sum, utils.GetLineSeparator())
+	fmt.Printf("Conditional sum: %d%s", conditionalSum, utils.GetLineSeparator())
+	fmt.Println("---------------")
+}
+
+func getTotalSum(input []string) int {
 	sum := 0
 	for _, line := range input {
 		indices := regexp.MustCompile(mainPattern).FindAllStringIndex(line, -1)
@@ -27,7 +37,31 @@ func (Day3) CompleteTask(input []string) {
 			sum += utils.StringAsInt(numExtract[0]) * utils.StringAsInt(numExtract[1])
 		}
 	}
-	fmt.Println("---------------")
-	fmt.Printf("Sum: %d%s", sum, utils.GetLineSeparator())
-	fmt.Println("---------------")
+	return sum
+}
+
+func getConditionalSum(input []string) int {
+	conditionalSum := 0
+	line := ""
+	for _, l := range input {
+		line += l
+	}
+	indices := regexp.MustCompile(conditionalMainPattern).FindAllStringIndex(line, -1)
+	skip := false
+	for _, operation := range indices {
+		action := line[operation[0]:operation[1]]
+		if action == "do()" {
+			skip = false
+		} else if action == "don't()" {
+			skip = true
+		} else {
+			numberIndices := regexp.MustCompile(numberPairPattern).FindAllStringIndex(action, -1)
+			numExtract := strings.Split(action[numberIndices[0][0]:numberIndices[0][1]], numberSeparator)
+			if !skip {
+				conditionalSum += utils.StringAsInt(numExtract[0]) * utils.StringAsInt(numExtract[1])
+			} else {
+			}
+		}
+	}
+	return conditionalSum
 }
